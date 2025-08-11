d384c7725806c895bbba73507aebac3d9855a834
Luis Bermudez <44736658+luisbs2412@users.noreply.github.com>
2025-08-11 17:40:29 +0000

    Modelos y relaciones listas

--- src/models.py
+++ src/models.py
@@ -1,19 +1,48 @@
+import enum
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey, Enum, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+class MediaType(enum.Enum):
+    IMAGE = "image"
+    VIDEO = "video"
+    AUDIO = "audio"
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String)
+    lastname: Mapped[str] = mapped_column(String)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    comment = db.relationship('Comment')
+    follower = db.relationship('Follower')
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String)
+    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False) 
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)  
+    comment = db.relationship('Comment') 
+    media = db.relationship('Media')
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[MediaType] = mapped_column(Enum(MediaType))
+    url: Mapped[str] = mapped_column(String)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+
+follower = Table(
+    "follower",
+    db.metadata,
+    Column("user_from_id", ForeignKey("user.id")),
+    Column("user_to_id", ForeignKey("user.id"))
+)
+
+
